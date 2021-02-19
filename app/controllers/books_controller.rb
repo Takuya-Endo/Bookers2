a8b240e09b706fdd9774a3853f230cd050c27328
Takuya-Endo <above5purple@gmail.com>
2021-02-19 22:39:02 +0000

    [Add]booksのeditとdestroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,9 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
+  end
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -9,7 +13,23 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user.id = @book.user_id
+    @user = User.find(@book.user_id)
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
