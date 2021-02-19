6838ecdb007f0dc2e5a59f08d262c541d455af4a
Takuya-Endo <above5purple@gmail.com>
2021-02-19 18:21:40 +0000

    [Fix]users_controllerのprovate以下:profile_imageより_id削除

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'aabfa1c7976fd59738be663f658b93053569c685dd51b2fcf6a2a08d66c12a58f863a399ac48623692612a6b3b60a3e535aa64b504b685a4392c3c7347d23a15'
