086502fca26fd76af600e30600db5d3ca65cabbf
Takuya-Endo <above5purple@gmail.com>
2021-02-20 15:37:35 +0000

    [Update]Bootstrap(bookedit,bookshow)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user = User.find(@book.user_id)
+    @user = User.find(current_user.id)
   end
 
   def edit
